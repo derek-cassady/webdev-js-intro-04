72ce0309aa50302391b7fef457acef8da41089c2
derek-cassady <derek@cassadyweb.com>
2025-01-11 23:06:20 -0500

    Completed experiment

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+// let healthPoints = 25;
+
+// if (healthPoints >= 10) {
+//     console.log("Hero is healthy.");
+// } else if (healthPoints < 10 && healthPoints >= 5) {
+//     console.log("Hero is slightly damaged.");
+// } else {
+//     console.log("Hero is close to death.");
+// }
+
+
+// let healthPoints = 25;
+
+// if (healthPoints <= 0) {
+//     console.log("Hero has fallen.");
+//     } else {
+//     console.log("Hero continues to fight.");
+//     }
+
+// let healthPoints = 25;
+
+// if (healthPoints > 10) {
+//     console.log("Hero is in excellent condition.");
+//     } else {
+//     console.log("Hero needs healing.");
+// }
